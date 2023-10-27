a6536fd0cc7a5501cef3b99e8a34a984c0ccc97f
Maksim_Alekseev <maxim040597@gmail.com>
2023-10-27 22:49:02 +0300

    fixed

--- src/UI/Menu/Menu.jsx
+++ src/UI/Menu/Menu.jsx
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import ItemsMenu from './ItemsMenu/ItemsMenu';
 import styles from './Menu.module.sass';
 import { useEffect } from 'react';
+import SubMenu from './SubMenu/Submenu';
 
 
 export default function Menu(props){
@@ -15,19 +16,46 @@ export default function Menu(props){
   }
 
   menuItems = useSelector(state => state.dataState.value[variation]);
-
+  let subItems = (item_id, parrent_id = null)=>{
+    for(let item of menuItems){
+      if(parrent_id == null){
+        <ItemsMenu variation={variation} key={item.id} item={item}/>
+      }
+      if(parrent_id != null){
+        if(parrent_id == item.id){
+          // console.log(1)
+          <ItemsMenu variation={variation} key={item.id} item={item}/>
+        }
+        
+      }
+      
+    }
+  }
 
   return (
       <nav className={styles[variation]}>
         <ul className={styles.menu_ul}>
-          
           {
             menuItems.map((item)=>
+              // subItems(item.id, item.parrent_item_id)
               
-              <ItemsMenu variation={variation} key={item.id} item={item}/>
+                (item.parrent_item_id == null)?
+                  <ItemsMenu variation={variation} key={item.id} item={item}/>
+                
+                :
+                (item.parrent_item_id != null)?
+                  (item.parrent_item_id == item.id)?
+                    // console.log(1)
+                    <ItemsMenu variation={variation} key={item.id} item={item}/>
+                    :
+                    ""
+                :
+                ""
+
+                
+              
             )
           }
-
         </ul>
       </nav>
   )
---
+++ src/UI/Menu/SubMenu/Submenu.jsx
@@ -0,0 +1,9 @@
+import styles from './SubMenu.module.sass';
+
+export default function SubMenu(props){
+    return(
+        <div className={styles.menu_ul}>
+            SubMenu
+        </div>
+    )
+}
