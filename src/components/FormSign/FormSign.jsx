da3a64bee872fb0f34f63521f06312f5e0603e56
Xpyst <qjq3@mail.ru>
2023-11-09 00:01:49 +0300

    ready login and register

--- src/components/FormSign/FormSign.jsx
+++ src/components/FormSign/FormSign.jsx
@@ -1,12 +1,21 @@
 import { Link } from 'react-router-dom';
-import Button from '../../UI/Button/Button';
 import style from './FormSign.module.sass';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faVk } from '@fortawesome/free-brands-svg-icons';
-
+import { useEffect, useState } from 'react';
+import SelectSign from './SelectSign/SelectSign'
 
 
 export default function FormSign(props) {
+
+  let [stateStatus, stateStatusState] = useState({params: 'sign'});
+
+  function changeStatus(param){
+    console.log(param)
+    stateStatusState(Object.assign({}, {params: param}) );
+  }
+
+  // useEffect(()=>{
+  //   // console.log(stateStatus)
+  // }, [stateStatus])
 
   return (
     <div id={style.signWrapp}>
@@ -14,22 +23,22 @@ export default function FormSign(props) {
           <div className={style.controlName__selection}>
 
             <div className={style.wrapp_input}>
-              <input className={style.input} id="ragio_sing" type='radio' checked value="sign" name="sign"/>
+              <input onChange ={ (evt)=>{  changeStatus(evt.target.value) } } className={style.input} id="ragio_sing" type='radio' value="sign" name="sign" defaultChecked/>
               <label className={style.label} for="ragio_sing">
                 Вход с паролем
               </label>
             </div>
 
             <div className={style.wrapp_input}>
-              <input className={style.input} id="ragio_reg" type='radio' value="reg" name="sign"/>
-              <label className={style.label} for="ragio_reg" >
-                Регистрация
+              <input onChange ={ (evt)=>{ changeStatus(evt.target.value) } } className={style.input} id="ragio_reg" type='radio' value="reg" name="sign"/>
+              <label className={style.label} for="ragio_reg">
+                Регистрация 
               </label>
             </div>
 
           </div>
 
-          <div className={style.controlName__selection_login}>
+          {/* <div className={style.controlName__selection_login}>
 
             <div className={style.loginWrapp__label}>
               <label for="desc_login" className={style.loginWrapp__label_label} >
@@ -52,19 +61,19 @@ export default function FormSign(props) {
           <div className={style.controlName__selection_login}>
 
             <div className={style.loginWrapp__label}>
-              <label for="desc_login" className={style.loginWrapp__label_label}  >
+              <label for="desc_pass" className={style.loginWrapp__label_label}  >
                 Пароль
               </label>
             </div>
 
             <div className={style.loginWrapp__input}>
-              <input id="desc_login" type='password' value="" className={style.loginWrapp__input_input}/>
+              <input id="desc_pass" type='password' value="" className={style.loginWrapp__input_input}/>
             </div>
     
           </div>
 
           <div className={style.submitBlock}>
-            <Button name={'Войти с Паролем'} type={'submit'} name_class={'button__form_sign'}/>
+            <Button name={'Войти с паролем'} type={'submit'} name_class={'button__form_sign'}/>
             <Link className={style.link_vk} to={'#'}>
               <span className={style.brendVK__wrapp}>
                 <FontAwesomeIcon className={style.brendVK__wrapp_icon} icon={faVk}/>
@@ -77,16 +86,18 @@ export default function FormSign(props) {
               <Link className={style.sign_form__link} to={'#'}>
                 напомнить пароль
               </Link>
-          </div>
+          </div> */}
 
+          <SelectSign status={stateStatus}/>
+          
         </form>
 
         <div className={style.privacyPolice}>
-        При входе вы принимаете 
-        <Link className={style.privacyPolice_link} to="#"> условия использования </Link> 
-        сайта и соглашаетесь на обработку 
-        персональных данных согласно 
-        <Link className={style.privacyPolice_link} to="#"> политике конфиденциальности</Link>.
+        При входе вы принимаете&nbsp;
+        <Link className={style.privacyPolice_link} to="#">условия использования</Link> 
+        &nbsp;сайта и соглашаетесь на обработку 
+        персональных данных согласно&nbsp;
+        <Link className={style.privacyPolice_link} to="#">политике конфиденциальности</Link>.
         </div>
     </div>
   )
