17d8c3884f1ca1dfec3e942121119a43d6f4250a
Xpyst <qjq3@mail.ru>
2023-12-04 16:07:39 +0300

    ready radio button

--- src/components/RadioButtonBootstrap/RadioButtonBootstrap.jsx
+++ src/components/RadioButtonBootstrap/RadioButtonBootstrap.jsx
@@ -1,35 +1,37 @@
 import { useState } from 'react';
 import style from './RadioButtonBootstrap.module.sass';
+import './RadioButtonBootstrap.css';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import ToggleButton from 'react-bootstrap/ToggleButton';
 
-export default function RadioButtonBootstrap(){
-    const [checked, setChecked] = useState(false);
-    const [radioValue, setRadioValue] = useState('1');
+export default function RadioButtonBootstrap(props){
+    const [checked, setChecked] = useState(true);
+    let defaultStatus = props.defaultStatus ?? 'all'
+    const [radioValue, setRadioValue] = useState(defaultStatus);
   
     const radios = [
-      { name: 'Любой', value: 'Любой' },
-      { name: 'Левый', value: 'Левый' },
-      { name: 'Правый', value: 'Правый' },
+      { name: 'Любой', value: 'all' },
+      { name: 'Левый', value: 'left' },
+      { name: 'Правый', value: 'right' },
     ];
 
     return (
         <>
-        <ButtonGroup>
-            {radios.map((radio, idx) => (
-                <ToggleButton
-                    key={idx}
-                    id={`radio-${idx}`}
-                    type="radio"
-                    name="radio"
-                    value={radio.value}
-                    checked={radioValue === radio.value}
-                    onChange={(e) => setRadioValue(e.currentTarget.value)}
-                >
-                    {radio.name}
-                </ToggleButton>
-            ))}
-        </ButtonGroup>
+            <ButtonGroup bsPrefix={style.Wrapp__radioButton}>
+                {radios.map((radio, idx) => (
+                    <ToggleButton
+                        key={idx}
+                        id={`radio-${idx}`}
+                        type="radio"
+                        name="radio"
+                        value={radio.value}
+                        checked={radioValue === radio.value}
+                        onChange={(e) => setRadioValue(e.currentTarget.value)}
+                    >
+                        {radio.name}
+                    </ToggleButton>
+                ))}
+            </ButtonGroup>
         </>
     )
 
