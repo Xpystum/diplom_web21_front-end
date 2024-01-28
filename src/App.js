23dcfea237dd63839e80468702ec994dbb068fce
Xpystum <qjq3@mail.ru>
2024-01-28 14:53:14 +0300

    testing redux persist

--- src/App.js
+++ src/App.js
@@ -98,12 +98,6 @@ function App() {
 
     },[])
 
-    
-
-
-
-
-
     return (
         <div className="App">
             {
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,27 @@
-import { configureStore, combineReducer } from '@reduxjs/toolkit'
+import { configureStore, combineReducers , getDefaultMiddleware  } from '@reduxjs/toolkit'
 import dataSliceReducer from './dataState';
 
 //redux persist
-import { persistStore, persistReducer } from 'redux-persist'
+import { 
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+ } from 'redux-persist'
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 
-import todoReducer from './reducers'
 
-const rootReducer = combineReducer({
-  todos: todoReducer,
+const rootReducer = combineReducers({
+  dataState: dataSliceReducer,
 });
+
+// const customizedMiddleware = getDefaultMiddleware({
+//   serializableCheck: false
+// })
 
 const persistConfig = {
   key: 'user',
@@ -19,9 +31,14 @@ const persistConfig = {
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: {
-    dataState: dataSliceReducer
-  }
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+  getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
 })
 
+export const persistor = persistStore(store);
 export default store
