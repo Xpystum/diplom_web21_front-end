670875991af0c444388e21b5c387e119a4c05cac
Xpyst <qjq3@mail.ru>
2024-02-10 01:39:23 +0300

    ready beggin page PrivateMessages

--- src/widgets/Chat/PrivateMessagesUser/PrivateMessagesUser.jsx
+++ src/widgets/Chat/PrivateMessagesUser/PrivateMessagesUser.jsx
@@ -11,12 +11,30 @@ export default function PrivateMessagesUser(props){
     return (
         <div className={style.wrappBlock}>
             <div className={style.wrappBlockInner}>
-                <div className={style.leftBlock_messages}>
+                <section className={style.leftBlock_messages}>
+                    <nav className={style.selectStack}>
+                        <div className={style.selectSwitch}>
+                            <span>Сообщения</span>
+                        </div>
+                    </nav>  
+                    <div className={style.emptyMessage}>
+                        <div className={style.wraapIcon}>
+                            <div className={style.icon}></div>
+                        </div>
+                        <h2>Нет Сообщений</h2>
+                        <p className={style.text}>
+                            Общайтесь с продавцами и покупателями,
+                            уточняйте интересующую информацию о
+                            товарах и услугах.
+                        </p>
+                    </div>
+                </section>
+                <section className={style.RightBlock_chat}>
+                    <div className={style.wrappChatEmpty}>
+                        <span>Выберите диалог из списка слева</span>
+                    </div>
                     {/* <Chat /> */}
-                </div>
-                <div className={style.RightBlock_chat}>
-
-                </div>
+                </section>
             </div>
         </div>
     )
